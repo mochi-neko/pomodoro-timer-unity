a5a35b6074e64ff178d2c22c13531d9c029f1fc3
mochineko <t.o.e.4315@gmail.com>
2019-09-05 23:34:41 +0900

    Separate timer logic

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,86 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
 
 namespace Mochineko.Pomodoro
 {
-	public class Timer : MonoBehaviour
+	public class Timer : IDisposable
 	{
-		[SerializeField]
-		private int spanMinutes = 5;
-		private System.TimeSpan SpanMinutes
-			=> new System.TimeSpan(
-				hours: 0,
-				minutes: spanMinutes,
-				seconds: 0
-			);
-		[SerializeField]
-		private Text remaiginTime;
-		private void DisplaySpanMinutes()
-			=> remaiginTime.text = SpanMinutes.ToString(timeFormat);
-		[SerializeField]
-		private Text switchText;
+		private readonly TimeSpan duration;
+		private readonly System.Diagnostics.Stopwatch stopwatch;
 
-		private System.Diagnostics.Stopwatch stopwatch 
-			= new System.Diagnostics.Stopwatch();
+		private readonly TimeSpan OneSecond
+			= new TimeSpan(hours: 0, minutes: 0, seconds: 1);
 
-		private const string timeFormat = @"hh\:mm\:ss";
-
-		[ContextMenu(nameof(Switch))]
-		public void Switch()
+		public Timer(TimeSpan duration)
 		{
-			if (!stopwatch.IsRunning)
-			{
-				StartTimer();
+			this.duration = duration;
+			this.stopwatch = new System.Diagnostics.Stopwatch();
 
-				switchText.text = "Stop";
-			}
-			else
-			{
-				StopTimer();
-
-				switchText.text = "Start";
-				DisplaySpanMinutes();
-			}
+			stopwatch.Start();
 		}
 
-		public void StartTimer()
+		public TimeSpan Elapsed
+			=> stopwatch.Elapsed;
+
+		public TimeSpan Remaining
+			=> duration.Subtract(Elapsed);
+
+		public TimeSpan RemainingRoundedUp
+			=> duration
+				.Subtract(Elapsed)
+				.Add(OneSecond);
+
+		public bool IsOver
+			=> Remaining.TotalMilliseconds <= 0;
+
+		public void Dispose()
 		{
-			stopwatch.Restart();
-		}
-
-		public void StopTimer()
-		{
-			stopwatch.Reset();
-		}
-
-		private void Start()
-		{
-			DisplaySpanMinutes();
-		}
-
-		private void Update()
-		{
-			if (!stopwatch.IsRunning)
-			{
-				return;
-			}
-
-			var remaining = 
-				SpanMinutes
-				.Subtract(stopwatch.Elapsed);
-
-			if (remaining.Ticks <= 0)
-			{
-				remaiginTime.text = "Finished";
-				return;
-			}
-
-			remaiginTime.text = 
-				remaining
-				.Add(new System.TimeSpan(hours: 0, minutes: 0, seconds: 1))
-				.ToString(timeFormat);
+			stopwatch.Stop();
 		}
 	}
 }
---
+++ Assets/Scripts/TimerBehaviour.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mochineko.Pomodoro
+{
+	public class TimerBehaviour : MonoBehaviour
+	{
+		[SerializeField]
+		private int spanMinutes = 1;
+		private System.TimeSpan SpanMinutes
+			=> new System.TimeSpan(
+				hours: 0,
+				minutes: spanMinutes,
+				seconds: 0
+			);
+		[SerializeField]
+		private Text remaiginTime;
+		private void DisplaySpanMinutes()
+			=> remaiginTime.text = SpanMinutes.ToString(timeFormat);
+		[SerializeField]
+		private Text switchText;
+
+		private Timer timer = null;
+
+		private const string timeFormat = @"hh\:mm\:ss";
+
+		[ContextMenu(nameof(Switch))]
+		public void Switch()
+		{
+			if (timer == null)
+			{
+				StartTimer();
+
+				switchText.text = "Stop";
+			}
+			else
+			{
+				StopTimer();
+
+				switchText.text = "Start";
+				DisplaySpanMinutes();
+			}
+		}
+
+		public void StartTimer()
+		{
+			if (timer != null)
+			{
+				StopTimer();
+			}
+
+			timer = new Timer(SpanMinutes);
+		}
+
+		public void StopTimer()
+		{
+			timer.Dispose();
+			timer = null;
+		}
+
+		private void Start()
+		{
+			DisplaySpanMinutes();
+		}
+
+		private void Update()
+		{
+			if (timer == null)
+			{
+				return;
+			}
+
+			if (timer.IsOver)
+			{
+				StopTimer();
+				switchText.text = "Start";
+				DisplaySpanMinutes();
+
+				return;
+			}
+
+			remaiginTime.text = timer.RemainingRoundedUp.ToString(timeFormat);
+		}
+	}
+}
