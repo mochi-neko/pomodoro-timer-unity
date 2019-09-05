6031ce33d1957a58769ee9be29846ee88b61eab9
watanabe.masaki <watanabe.masaki@synamon.jp>
2019-09-05 18:38:40 +0900

    Add namespace

--- Assets/Scripts/Stopwatch.cs
+++ Assets/Scripts/Stopwatch.cs
@@ -3,77 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Stopwatch : MonoBehaviour
+namespace Mochineko.Pomodoro
 {
-	[SerializeField]
-	private Text elapsedTime;
-
-	[SerializeField]
-	private Text switchText;
-	private void DisplayStartText()
+	public class Stopwatch : MonoBehaviour
 	{
-		switchText.text = "Start";
-	}
-	private void DisplayStopText()
-	{ 
-		switchText.text = "Stop";
-	}
+		[SerializeField]
+		private Text elapsedTime;
 
-	private System.Diagnostics.Stopwatch stopwatch
-		= new System.Diagnostics.Stopwatch();
-
-	private const string timeFormat = @"hh\:mm\:ss";
-
-	[ContextMenu(nameof(SwitchStartAndStop))]
-	public void SwitchStartAndStop()
-	{
-		if (!stopwatch.IsRunning)
+		[SerializeField]
+		private Text switchText;
+		private void DisplayStartText()
 		{
-			StartTime();
-
-			DisplayStopText();
+			switchText.text = "Start";
 		}
-		else
+		private void DisplayStopText()
 		{
-			StopTime();
+			switchText.text = "Stop";
+		}
+
+		private System.Diagnostics.Stopwatch stopwatch
+			= new System.Diagnostics.Stopwatch();
+
+		private const string timeFormat = @"hh\:mm\:ss";
+
+		[ContextMenu(nameof(SwitchStartAndStop))]
+		public void SwitchStartAndStop()
+		{
+			if (!stopwatch.IsRunning)
+			{
+				StartTime();
+
+				DisplayStopText();
+			}
+			else
+			{
+				StopTime();
+
+				DisplayStartText();
+			}
+		}
+
+		[ContextMenu("Start")]
+		public void StartTime()
+		{
+			stopwatch.Start();
+		}
+
+		[ContextMenu("Stop")]
+		public void StopTime()
+		{
+			stopwatch.Stop();
+		}
+
+		[ContextMenu("Reset")]
+		public void ResetTime()
+		{
+			stopwatch.Reset();
 
 			DisplayStartText();
 		}
-	}
 
-	[ContextMenu("Start")]
-	public void StartTime()
-	{
-		stopwatch.Start();
-	}
+		[ContextMenu("Restart")]
+		public void RestartTime()
+		{
+			stopwatch.Restart();
+		}
 
-	[ContextMenu("Stop")]
-	public void StopTime()
-	{
-		stopwatch.Stop();
-	}
+		private void Start()
+		{
+			elapsedTime.text = "00:00:00";
+		}
 
-	[ContextMenu("Reset")]
-	public void ResetTime()
-	{
-		stopwatch.Reset();
-
-		DisplayStartText();
-	}
-
-	[ContextMenu("Restart")]
-	public void RestartTime()
-	{
-		stopwatch.Restart();
-	}
-
-	private void Start()
-	{
-		elapsedTime.text = "00:00:00";
-	}
-
-	private void Update()
-	{
-		elapsedTime.text = stopwatch.Elapsed.ToString(timeFormat);
+		private void Update()
+		{
+			elapsedTime.text = stopwatch.Elapsed.ToString(timeFormat);
+		}
 	}
 }
