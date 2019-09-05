9da06071e9d4beb4b41ce4753d77db846a181495
watanabe.masaki <watanabe.masaki@synamon.jp>
2019-09-05 18:35:42 +0900

    Make a simple stopwatch

---
+++ Assets/Scripts/Stopwatch.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stopwatch : MonoBehaviour
+{
+	[SerializeField]
+	private Text elapsedTime;
+
+	[SerializeField]
+	private Text switchText;
+	private void DisplayStartText()
+	{
+		switchText.text = "Start";
+	}
+	private void DisplayStopText()
+	{ 
+		switchText.text = "Stop";
+	}
+
+	private System.Diagnostics.Stopwatch stopwatch
+		= new System.Diagnostics.Stopwatch();
+
+	private const string timeFormat = @"hh\:mm\:ss";
+
+	[ContextMenu(nameof(SwitchStartAndStop))]
+	public void SwitchStartAndStop()
+	{
+		if (!stopwatch.IsRunning)
+		{
+			StartTime();
+
+			DisplayStopText();
+		}
+		else
+		{
+			StopTime();
+
+			DisplayStartText();
+		}
+	}
+
+	[ContextMenu("Start")]
+	public void StartTime()
+	{
+		stopwatch.Start();
+	}
+
+	[ContextMenu("Stop")]
+	public void StopTime()
+	{
+		stopwatch.Stop();
+	}
+
+	[ContextMenu("Reset")]
+	public void ResetTime()
+	{
+		stopwatch.Reset();
+
+		DisplayStartText();
+	}
+
+	[ContextMenu("Restart")]
+	public void RestartTime()
+	{
+		stopwatch.Restart();
+	}
+
+	private void Start()
+	{
+		elapsedTime.text = "00:00:00";
+	}
+
+	private void Update()
+	{
+		elapsedTime.text = stopwatch.Elapsed.ToString(timeFormat);
+	}
+}
