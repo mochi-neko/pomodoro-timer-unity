ff3bc7c63f1b40a88a6a72bf85d6f1eed6a617b8
watanabe.masaki <watanabe.masaki@synamon.jp>
2019-09-05 17:29:58 +0900

    Make a simple clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mochineko.Pomodoro
+{
+	public class Clock : MonoBehaviour
+	{
+		[SerializeField]
+		private Text text;
+
+		private void Update()
+		{
+			text.text = System.DateTime.Now.ToString();
+		}
+
+	}
+}
