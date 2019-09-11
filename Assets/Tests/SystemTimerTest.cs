d728d3e67a5b5538c2bce2421e2ad4ab90a962f4
mochineko <t.o.e.4315@gmail.com>
2019-09-11 19:38:44 +0900

    Add the test of System.Timers.Timer

---
+++ Assets/Tests/SystemTimerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Mochineko.Pomodoro.Tests
+{
+	[TestFixture]
+	public class SystemTimerTest
+	{
+		[TestCase(3)]
+		[TestCase(2)]
+		[TestCase(1)]
+		public async void IntervalTest(int interval)
+		{
+			using (var timer = new System.Timers.Timer(interval))
+			{
+				var hasBeenTimeOver = false;
+				timer.Elapsed += (sender, argument) => hasBeenTimeOver = true;
+
+				Assert.IsFalse(hasBeenTimeOver);
+
+				timer.Start();
+				await Task.Delay(interval);
+
+				Assert.IsTrue(hasBeenTimeOver);
+
+				timer.Stop();
+			}
+		}
+	}
+}
