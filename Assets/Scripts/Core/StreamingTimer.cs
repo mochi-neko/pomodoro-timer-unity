33c7dfa07300a95ebe2c8500971f89d0dc4e1c09
mochineko <t.o.e.4315@gmail.com>
2019-09-13 15:28:58 +0900

    Make StreamingTimer and its test

---
+++ Assets/Scripts/Core/StreamingTimer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Mochineko.Pomodoro
+{
+	public class StreamingTimer : IDisposable
+	{
+		public ManagedEvent OnStart { get; } = new ManagedEvent();
+		public ManagedEvent<string> OnProcess = new ManagedEvent<string>();
+		public ManagedEvent OnElapsed { get; } = new ManagedEvent();
+		public ManagedEvent OnReset { get; } = new ManagedEvent();
+
+		private InstantialTimer timer;
+		private readonly TimeSpan span;
+		private const string format = @"mm\:ss";
+
+		public StreamingTimer(TimeSpan span)
+		{
+			if (span.Ticks <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(span));
+			}
+
+			this.span = span;
+		}
+
+		public void Start()
+		{
+			if (timer != null)
+			{
+				DisposeTimer();
+			}
+
+			timer = new InstantialTimer(span);
+
+			OnStart?.Invoke();
+		}
+
+		public void Process()
+		{
+			if (timer == null)
+			{
+				return;
+			}
+
+			if (!timer.IsOver)
+			{
+				OnProcess?.Invoke(
+					timer
+					.RemainingSecoundsRoundedUp
+					.ToString(format)
+				);
+				return;
+			}
+
+			OnElapsed?.Invoke();
+
+			DisposeTimer();
+		}
+
+		public void Reset()
+		{
+			if (timer == null)
+			{
+				return;
+			}
+
+			DisposeTimer();
+
+			OnReset?.Invoke();
+		}
+
+		public void Dispose()
+		{
+			DisposeTimer();
+
+			OnStart.Dispose();
+			OnReset?.Dispose();
+		}
+
+		private void DisposeTimer()
+		{
+			timer.Dispose();
+			timer = null;
+		}
+	}
+}
---
+++ Assets/Tests/StreamingTimerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Mochineko.Pomodoro.Tests
+{
+	[TestFixture(TestOf = typeof(StreamingTimer))]
+	public class StreamingTimerTest
+	{
+		[Test]
+		public async void ElapsTest()
+		{
+			var span = new TimeSpan(0, 0, 0, 0, 1);
+			var timer = new StreamingTimer(span);
+			var flag = false;
+
+			timer.OnElapsed.Add(() => flag = true);
+
+			timer.Start();
+
+			await Task.Delay(span);
+
+			timer.Process();
+
+			Assert.IsTrue(flag);
+		}
+	}
+}
